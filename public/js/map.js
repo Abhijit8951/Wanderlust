33f96d18459039c784c9d212c74c1abfc36428b7
Abhijit swain <swain.abhijit0119@gmail.com>
2024-12-17 18:42:19 +0530

    Fixed coordionates issue

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,14 @@
-
-	mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: "mapbox://styles/mapbox/streets-v12",
-        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-    });
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 10 // starting zoom
+});
 
 const marker = new mapboxgl.Marker({ color: "red" })
-.setLngLat(listing.geometry.coordinates)
-.setPopup(new mapboxgl.Popup({offset: 25,})
-.setHTML(`<h7>${listing.title}</h7><br><p>Exact Location Provided After Booking</p>`)
+.setLngLat(coordinates)
+.setPopup(new mapboxgl.Popup({offset: 25})
+.setHTML(`<p>Exact Location Provided After Booking</p>`)
 .setMaxWidth("300px"))
 .addTo(map);
